b77864b33f62b0d961238a8c315899c421e8fad3
aram0117 <dkfka568355@gmail.com>
2025-10-22 20:44:52 +0900

    feature: 레벨2단계 개발완료

---
+++ src/LV2/Main.java
@@ -0,0 +1,75 @@
+package LV2;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 메뉴 이름, 가격, 설명이 들어간 메뉴아이템 인스턴스
+        MenuItem menu1 = new MenuItem("1.ShackBurger", "W6.9", "토마토, 양상추, 쉑소스가 토핑된 치즈버거");
+        MenuItem menu2 = new MenuItem("2.SmokeShack", "W8.9", "베이컨, 체리 페퍼에 쉑소스가 토핑된 치즈버거");
+        MenuItem menu3 = new MenuItem("3.Cheeseburger", "W6.9", "포테이토 번과 비프패티, 치즈가 토핑된 치즈버거");
+        MenuItem menu4 = new MenuItem("4.Hamburger", "W5.4", "비프패티를 기반으로 야채가 들어간 기본버거");
+
+        // list of로 lISt에 메뉴삽입
+        List<MenuItem> menuItemList = new ArrayList<>(List.of(menu1, menu2, menu3, menu4));
+
+        // 스캐너 선언
+        Scanner sc = new Scanner(System.in);
+
+        // 스위치문에 주문번호 입력조건과 주문실행후 continue로 while문 재실행
+        // 0을 입력하면 break로 반복문종료
+        while (true) {
+            System.out.println("[ SHAKESHACK MENU ]");
+            // for each문으로 메뉴아이템 하나씩 출력
+            for (MenuItem menuItem : menuItemList) {
+                System.out.println(menuItem.name + " [가격]" + menuItem.price + " [설명]" + menuItem.explanation);
+            }
+            System.out.println("0. 종료      | 종료");
+
+            System.out.println("<메뉴번호를 입력하세요>");
+            int input = sc.nextInt();
+
+            // 각 주문번호 입력 조건에 해당하는 메뉴 실행
+            switch (input) {
+                case 1 -> {
+                    System.out.println(menu1.name + " | " + menu1.price + " | " + menu1.explanation);
+                    System.out.println("[주문이 완료되었습니다]");
+                    System.out.println("--------------------------------------");
+                    continue;
+                }
+                case 2 -> {
+                    System.out.println(menu2.name + " | " + menu2.price + " | " + menu2.explanation);
+                    System.out.println("[주문이 완료되었습니다]");
+                    System.out.println("--------------------------------------");
+                    continue;
+                }
+                case 3 -> {
+                    System.out.println(menu3.name + " | " + menu3.price + " | " + menu3.explanation);
+                    System.out.println("[주문이 완료되었습니다]");
+                    System.out.println("--------------------------------------");
+                    continue;
+                }
+                case 4 -> {
+                    System.out.println(menu4.name + " | " + menu4.price + " | " + menu4.explanation);
+                    System.out.println("[주문이 완료되었습니다]");
+                    System.out.println("--------------------------------------");
+                    continue;
+                }
+                case 0 -> {
+                    // 스위치문 종료
+                    break;
+                }
+                default -> {
+                    System.out.println("잘못된 번호입니다 다시 메뉴번호를 입력해주세요");
+                    System.out.println("--------------------------------------");
+                    continue;
+                }
+            }
+            // 반복문 종료
+            System.out.println("키오스크를 종료됩니다");
+            break;
+        }
+    }
+}
---
+++ src/LV2/MenuItem.java
@@ -0,0 +1,18 @@
+package LV2;
+
+public class MenuItem {
+    // 속성
+    String name;
+    String price;
+    String explanation;
+
+    // 생성자
+    MenuItem(String name, String price, String explanation) {
+        this.name = name;
+        this.price = price;
+        this.explanation = explanation;
+    }
+}
+
+
+
